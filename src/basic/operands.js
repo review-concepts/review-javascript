8d66feac0c9b7bfe66042df331de470302f9cd63
Ramin Rostami <rm.rostami@gmail.com>
2020-07-18 10:39:37 +0430

    operands

---
+++ src/basic/operands.js
@@ -0,0 +1,113 @@
+/**
+ *  javascipt operators:
+ *      arithmatic
+ */
+
+// let x = 10;
+// let y = 3;
+//
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y)
+// console.log(x % y)
+// console.log(x ** y)
+//
+// // Increament
+// console.log(x++)
+// console.log(x)
+//
+// // Decreament
+// console.log(x--)
+// console.log(x)
+
+
+
+/*
+*   Assignment Operator
+*/
+
+// let x = 10;
+// x = x + 5;
+// x+=5;
+
+/*
+*   Comparison Operator
+*/
+
+// let  x = 1;
+//
+// // Relational
+// console.log(x > 0)
+// console.log(x > 1)
+// console.log(x >= 1)
+//
+// // Equility
+// console.log(x === 1)
+// console.log(x !== 1)
+//
+// let x = 1
+// // Strict Equlity
+// console.log(1 ==='1') // strict equlity type and value
+// // Lose Equility
+// console.log(1 == '1') // just value not type
+
+
+/*
+*   Ternary Operator
+*/
+// let points = 110;
+// let type = points > 100 ? 'gold': 'silver';
+// console.log(type)
+
+/*
+*   Logical Operator
+*/
+
+// logical AND && : if both operands are true
+// console.log(true && true)
+
+// logical OR ||
+// return true if one pf the operand is TRUE
+
+//  let highIncome = true;
+//  let goodCreditScore = false;
+//  let loan = highIncome && goodCreditScore
+// console.log(loan)
+
+// NOT !
+// console.log(!true)
+
+// logical operators with non-booleans
+/*
+    Falsy:
+        undefined
+        null
+        0
+        false
+        ''
+        NaN
+     Truthy: Anything that is not Falsy
+ */
+
+// test these lines on chrome console as javascript engine
+
+/*
+false || true
+true
+false || 'ramin'
+"ramin"
+false || 1
+1
+false|| 10||2||5||7
+ */
+
+// we can use that in our program like blew:
+
+// let userColor = false
+// let defaultColor = 'blue'
+//
+// let currentColor = userColor || defaultColor
+// console.log(currentColor)
+
+
